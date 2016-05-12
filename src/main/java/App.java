cc56f8a1f990ec35bcbe5fb95d0fd41be566b0f6
Elissa Sosland and Samuel Durfey <samuel@>
2016-05-12 09:32:00 -0700

    Add recipes page. It will list all recipes. test passes. Adds test for adding a tag, not yet passing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
   public static void main(String[] args) {
@@ -13,6 +14,14 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/recipes", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/recipes.vtl");
+      List<Recipe> recipes = Recipe.all();
+      model.put("recipes", recipes);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,14 @@
 
+import org.sql2o.*;
+import org.junit.*;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import static org.fluentlenium.core.filter.FilterConstructor.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.junit.Assert.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -19,9 +21,29 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Task list!");
-  // }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Recipe Box!");
+  }
+
+  @Test
+  public void recipesAreListedTest() {
+    Recipe testRecipe = new Recipe("Burrito", "burrito stuff", "wrap it up and eat it.");
+    testRecipe.save();
+    goTo("http://localhost:4567/recipes");
+    assertThat(pageSource()).contains("Burrito");
+  }
+
+  @Test
+  public void tagIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Tags"));
+    fill("#name").with("Desserts");
+    submit(".btn");
+    assertThat(pageSource()).contains("Desserts");
+  }
 }
